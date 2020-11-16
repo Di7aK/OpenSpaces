afa4ae42ce699d30ac6a8e1c4de17ae00cf3e50e
Di7aK <di7ok@spaces.ru>
2020-11-16 14:48:58 +0500

    фикус пустогол камента

--- app/src/main/java/com/di7ak/openspaces/utils/CommentParser.kt
+++ app/src/main/java/com/di7ak/openspaces/utils/CommentParser.kt
@@ -1,22 +1,25 @@
 package com.di7ak.openspaces.utils
 
 import android.util.Log
-import com.di7ak.openspaces.data.entities.Attach
 import java.util.regex.Pattern
 
 class CommentParser {
     companion object {
-        private val pattern =
-            Pattern.compile("<span itemprop=\"text\">(.+?)</span>", Pattern.DOTALL)
+        private val pattern = Pattern.compile("<span itemprop=\"text\">(.+?)</span>", Pattern.DOTALL)
+        private val pattern2 = Pattern.compile("<div class=\"text-block11 nl\" itemprop=\"text\">(.+?)</div>", Pattern.DOTALL)
 
         fun parse(source: String): String {
             try {
-                val matcher = pattern.matcher(source)
+                var matcher = pattern.matcher(source)
+                while (matcher.find()) {
+                    return matcher.group(1) ?: ""
+                }
+                matcher = pattern2.matcher(source)
                 while (matcher.find()) {
                     return matcher.group(1) ?: ""
                 }
             } catch (e: Exception) {
-                Log.d("lol", "", e)
+                e.printStackTrace()
             }
             return ""
         }
