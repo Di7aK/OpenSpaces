e4866cd9e5048247bca32c6dfe9dc5fa88f5c9c9
Di7aK <di7ok@spaces.ru>
2020-11-17 16:16:27 +0500

    3й тег кома

--- app/src/main/java/com/di7ak/openspaces/utils/CommentParser.kt
+++ app/src/main/java/com/di7ak/openspaces/utils/CommentParser.kt
@@ -6,7 +6,8 @@ import java.util.regex.Pattern
 class CommentParser {
     companion object {
         private val pattern = Pattern.compile("<span itemprop=\"text\">(.+?)</span>", Pattern.DOTALL)
-        private val pattern2 = Pattern.compile("<div class=\"text-block11 nl\" itemprop=\"text\">(.+?)</div>", Pattern.DOTALL)
+        private val pattern2 = Pattern.compile("<div itemprop=\"text\">(.+?)</div>", Pattern.DOTALL)
+        private val pattern3 = Pattern.compile("<div class=\"text-block11 nl\" itemprop=\"text\">(.+?)</div>", Pattern.DOTALL)
 
         fun parse(source: String): String {
             try {
@@ -15,6 +16,10 @@ class CommentParser {
                     return matcher.group(1) ?: ""
                 }
                 matcher = pattern2.matcher(source)
+                while (matcher.find()) {
+                    return matcher.group(1) ?: ""
+                }
+                matcher = pattern3.matcher(source)
                 while (matcher.find()) {
                     return matcher.group(1) ?: ""
                 }
