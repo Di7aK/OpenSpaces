e304dc7a3ece3fffd6f7b24bcb08bcad8aaabf00
Di7aK <di7ok@spaces.ru>
2020-11-17 16:21:51 +0500

    рефактор парсера кома

--- app/src/main/java/com/di7ak/openspaces/utils/CommentParser.kt
+++ app/src/main/java/com/di7ak/openspaces/utils/CommentParser.kt
@@ -1,25 +1,14 @@
 package com.di7ak.openspaces.utils
 
-import android.util.Log
 import java.util.regex.Pattern
 
 class CommentParser {
     companion object {
-        private val pattern = Pattern.compile("<span itemprop=\"text\">(.+?)</span>", Pattern.DOTALL)
-        private val pattern2 = Pattern.compile("<div itemprop=\"text\">(.+?)</div>", Pattern.DOTALL)
-        private val pattern3 = Pattern.compile("<div class=\"text-block11 nl\" itemprop=\"text\">(.+?)</div>", Pattern.DOTALL)
+        private val pattern = Pattern.compile("itemprop=\"text\">(.+?)</", Pattern.DOTALL)
 
         fun parse(source: String): String {
             try {
-                var matcher = pattern.matcher(source)
-                while (matcher.find()) {
-                    return matcher.group(1) ?: ""
-                }
-                matcher = pattern2.matcher(source)
-                while (matcher.find()) {
-                    return matcher.group(1) ?: ""
-                }
-                matcher = pattern3.matcher(source)
+                val matcher = pattern.matcher(source)
                 while (matcher.find()) {
                     return matcher.group(1) ?: ""
                 }
