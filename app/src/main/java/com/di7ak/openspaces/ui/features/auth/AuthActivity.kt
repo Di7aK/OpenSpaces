16aa60d22262301d1214007ee3c57395d08c3349
Di7aK <di7ok@spaces.ru>
2020-11-21 13:39:16 +0500

    антиpidersiя при кнопке смены акка в профиле

--- app/src/main/java/com/di7ak/openspaces/ui/features/auth/AuthActivity.kt
+++ app/src/main/java/com/di7ak/openspaces/ui/features/auth/AuthActivity.kt
@@ -15,30 +15,40 @@ import dagger.hilt.android.AndroidEntryPoint
 class AuthActivity : BaseActivity() {
     private val viewModel: AuthViewModel by viewModels()
     private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var binding: ActivityAuthBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityAuthBinding.inflate(layoutInflater)
+
+        binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
-
-        val navHostFragment: NavHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
-
-        val appBarConfiguration = AppBarConfiguration(navController.graph)
-        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
 
         setupObservers()
         viewModel.fetchSessions()
     }
 
+    private fun setupNavController(startDestination: Int) {
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        val graphInflater = navHostFragment.navController.navInflater
+        val graph = graphInflater.inflate(R.navigation.auth)
+        navController = navHostFragment.navController
+
+        graph.startDestination = startDestination
+        navController.graph = graph
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+    }
+
     private fun setupObservers() {
         viewModel.sessions.observe(this) {
-            if(it.isNotEmpty()) {
-                navController.navigate(R.id.action_authFragment_to_accountsFragment)
-            }
+            val destination = if (it.isNotEmpty()) R.id.navigationAccounts else R.id.navigationLogin
+
+            setupNavController(destination)
         }
     }
 }
